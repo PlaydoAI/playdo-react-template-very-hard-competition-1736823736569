170699e6be98f52eac031313d910d169910f3d92
PlaydoAI <gautham9a@outlook.com>
2025-01-13 22:02:25 -0500

    Copy src/App.js from template

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to the Hackathon Starter Template</h1>
+      <p>Edit <code>src/App.js</code> and save to reload.</p>
+    </div>
+  );
+}
+
+export default App;
